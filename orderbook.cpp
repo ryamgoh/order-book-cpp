22c755703394fc05999ac717b45624aacd9f7855
Ryann <ryanngohenrui@gmail.com>
2026-02-24 22:19:28 +0800

    Add Trades information

--- orderbook.cpp
+++ orderbook.cpp
@@ -124,3 +124,25 @@ private:
   Price price_;
   Quantity quantity_;
 };
+
+struct TradeInfo {
+  OrderId orderId_;
+  Price price_;
+  Quantity quantity;
+};
+
+class Trade {
+public:
+  Trade(const TradeInfo &bidTrade, const TradeInfo &askTrade)
+      : bidTrade_{bidTrade}, askTrade_{askTrade} {}
+
+  const TradeInfo &GetBidTrade() const { return bidTrade_; }
+  const TradeInfo &GetAskTrade() const { return askTrade_; }
+
+private:
+  TradeInfo bidTrade_;
+  TradeInfo askTrade_;
+};
+
+using Trades = std::vector<Trade>;
+
