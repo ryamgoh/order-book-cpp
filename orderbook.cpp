ba0f967646c89d2c38d12c9194b2126de8e42022
Ryann <ryanngohenrui@gmail.com>
2026-02-24 22:19:15 +0800

    Add DTO for OrderModify

--- orderbook.cpp
+++ orderbook.cpp
@@ -101,3 +101,26 @@ private:
 
 using OrderPointer = std::shared_ptr<Order>;
 using OrderPointers = std::list<OrderPointer>;
+
+class OrderModify {
+  // This is just a DTO for making changes to orders
+public:
+  OrderModify(OrderId orderId, Side side, Price price, Quantity quantity)
+      : orderId_{orderId}, price_{price}, side_{side}, quantity_{quantity} {}
+
+  OrderId GetOrderId() const { return orderId_; }
+  Side GetSide() const { return side_; }
+  Price GetPrice() const { return price_; }
+  Quantity GetQuantity() const { return quantity_; }
+
+  OrderPointer ToOrderPointer(OrderType type) const {
+    return std::make_shared<Order>(type, GetOrderId(), GetSide(), GetPrice(),
+                                   GetQuantity());
+  }
+
+private:
+  OrderId orderId_;
+  Side side_;
+  Price price_;
+  Quantity quantity_;
+};
