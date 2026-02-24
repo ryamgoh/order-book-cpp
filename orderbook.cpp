104292f4b00ea5e94cfcc20bb9b8a54d3dcf35f2
Ryann <ryanngohenrui@gmail.com>
2026-02-24 22:20:18 +0800

    add test in main function

--- orderbook.cpp
+++ orderbook.cpp
@@ -342,3 +342,15 @@ public:
     return OrderbookLevelInfos{bidInfos, askInfos};
   }
 };
+
+int main() {
+  Orderbook orderbook;
+  const OrderId orderId = 1;
+  orderbook.AddOrder(std::make_shared<Order>(OrderType::GoodTillCancel, orderId,
+                                             Side::Buy, 100, 10));
+  std::cout << orderbook.Size() << std::endl; // 1
+  orderbook.CancelOrder(orderId);
+  std::cout << orderbook.Size() << std::endl; // 0
+  return 0;
+}
+
